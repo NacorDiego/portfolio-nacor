a0dd36a87e9937eec521a7354753eeb380675347
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-12-29 12:08:09 -0300

    Creation of components 'about', 'home', 'projects', 'stack' and 'contact'. The routes to these components are set in 'app.routes'.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { StackComponent } from './components/stack/stack.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo:'/home', pathMatch: 'full' },
+    { path:'/home', component:HomeComponent },
+    { path:'/about', component:AboutComponent },
+    { path:'/projects', component:ProjectsComponent },
+    { path:'/stack', component:StackComponent },
+    { path:'contact', component:ContactComponent }
+];
